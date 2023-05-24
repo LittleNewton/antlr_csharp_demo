131dce6544bb0ccaabccec3ea7735c29cb04b843
Peng_Liu <littlenewton6@gmail.com>
2023-05-24 22:26:39 +0800

    [+] init

---
+++ src/antlr-csharp/MyCalculator.cs
@@ -0,0 +1,30 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Numerics;  
+
+namespace antlr_csharp
+{
+    class MyCalculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an arithmetic expression:");
+            string input = Console.ReadLine();
+
+            // Create a lexer and parser for the input expression
+            var inputStream = new AntlrInputStream(input);
+            var lexer = new CalculatorLexer(inputStream);
+            var commonTokenStream = new CommonTokenStream(lexer);
+            var parser = new CalculatorParser(commonTokenStream);
+
+            // Parse the input expression and retrieve the parse tree
+            var parseTree = parser.calc();
+
+            // Create a visitor instance and visit the parse tree
+            var visitor = new MyVisitor();
+            var result = visitor.Visit(parseTree);
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
---
+++ src/antlr-csharp/MyVisitor.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace antlr_csharp
+{
+    class MyVisitor : CalculatorBaseVisitor<int>
+    {
+        public override int VisitNumber(CalculatorParser.NumberContext context)
+        {
+            return int.Parse(context.NUM().GetText());
+        }
+
+        public override int VisitPlusOrMinusExpr(CalculatorParser.PlusOrMinusExprContext context)
+        {
+            int value = Visit(context.expr());
+            if (context.PLUS() != null)
+            {
+                return value;
+            }
+            else if (context.MINUS() != null)
+            {
+                return -1 * value;
+            }
+            else
+            {
+                throw new InvalidOperationException("Invalid operator");
+            }
+        }
+
+        public override int VisitInfixExpr(CalculatorParser.InfixExprContext context)
+        {
+            int left = Visit(context.left);
+            int right = Visit(context.right);
+            string op = context.op.Text;
+
+            if (op == "*")
+            {
+                return left * right;
+            }
+            else if (op == "/")
+            {
+                if (right != 0)
+                {
+                    return left / right;
+                }
+                else
+                {
+                    throw new InvalidOperationException("除数不能为 0");
+                }
+            }
+            else if (op == "+")
+            {
+                return left + right;
+            }
+            else if (op == "-")
+            {
+                return left - right;
+            }
+            else
+            {
+                throw new InvalidProgramException("What?");
+            }
+        }
+
+        public override int VisitParensExpr(CalculatorParser.ParensExprContext context)
+        {
+            return Visit(context.expr());
+        }
+    }
+}
